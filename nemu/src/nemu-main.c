c37b67c7a1720ad66aff739757f2eadabef87487
tracer-ysyx <tracer@ysyx.org>
2025-10-22 18:05:34 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:05:33 up 57 days, 18:58,  2 users,  load average: 5.50, 6.02, 6.15

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -29,7 +29,10 @@ int main(int argc, char *argv[]) {
 #endif
 
 #ifdef CONFIG_NPC_SO
+#ifndef CONFIG_TARGET_SHARE
     printf("你打开SHARED LIB选项了吗, 要产生NPC_SO需要同时打开NPC_SO以及SHARED LIB\n");
+    exit(1);
+#endif
 #endif
   /* Start engine. */
   engine_start();
